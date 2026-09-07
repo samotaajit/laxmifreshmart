dac0875f64b000f0e1d833eac61ca965b413eab5
Sunil <sky57934568@gmail.com>
2026-09-08 00:08:42 +0530

    Added error reporting

--- config/database.php
+++ config/database.php
@@ -1,12 +1,6 @@
 <?php
-// Report all PHP errors
-error_reporting(E_ALL);
-
-// Force errors to be displayed on the screen
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
 declare(strict_types=1);
+
 
 $host = 'mysql-18d0be72-samota4209211-e4d4.f.aivencloud.com';
 $dbname = 'laxmifreshmart';
@@ -22,11 +16,17 @@ try {
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
-        ]
-    );
-} catch (PDOException $e) {
-    error_log($e->getMessage());
-
-    http_response_code(500);
-    exit('Database connection failed.');
-}
+            ]
+            );
+            } catch (PDOException $e) {
+                error_log($e->getMessage());
+                
+                http_response_code(500);
+                exit('Database connection failed.');
+                }
+                // Report all PHP errors
+                error_reporting(E_ALL);
+                
+                // Force errors to be displayed on the screen
+                ini_set('display_startup_errors', 1);
+                ini_set('display_errors', 1);
